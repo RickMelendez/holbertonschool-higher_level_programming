0ac26bdeb15c95245187334bd82178491b67845d
RickMelendez <rickmelendez001@gmail.com>
2024-05-17 23:13:15 -0400

    added comments

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-"""Square Class
-
-This class don't do anything for the moment
-
-"""
+"""This module defines an empty class called Square"""
 
 
 class Square:
+    """Empty class"""
     pass

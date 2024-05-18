7c835c3bda8ab9928cf540daea8bd23e339d5eca
RickMelendez <rickmelendez001@gmail.com>
2024-05-17 23:21:16 -0400

    added comments

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,18 +3,25 @@
 
 
 class Square:
-   
-   
-    """Class Square
-        
-        Attributes:
-        Private instance attribute: size
-
-        """
+    """Class Square"""
+    
     def __init__(self, size):
-       
-       
         """Args:
         size: no type/value verification
         """
         self.__size = size
+
+if __name__ == "__main__":
+    my_square = Square(3)
+    print(type(my_square))
+    print(my_square.__dict__)
+
+    try:
+        print(my_square.size)
+    except AttributeError as e:
+        print(e)
+
+    try:
+        print(my_square.__size)
+    except AttributeError as e:
+        print(e)

be5f0e569cd7c7b00aa4780e83e06e271460cca1
RickMelendez <rickmelendez001@gmail.com>
2024-05-22 23:05:06 -0400

    added comments

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -6,5 +6,10 @@ defines a new class named BaseGeometry.
 
 class BaseGeometry:
     def area(self):
-
+        """
+        Define a public instance method named area.
+            """
         raise Exception(area() is not implemented)
+        """
+        raise an exeption.
+            """

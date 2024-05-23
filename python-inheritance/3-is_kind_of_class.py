50e12c2bdf3d43b26fc0c9150c24dca146c6de8c
RickMelendez <rickmelendez001@gmail.com>
2024-05-22 22:05:14 -0400

    fixed error on the if statement

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -8,7 +8,7 @@ def is_kind_of_class(obj, a_class):
     """
     Returns True if the object is class else return False.
         """
-    if isinstance(obj) is a_class:
+    if isinstance(obj, a_class):
         return True
     else:
         return False

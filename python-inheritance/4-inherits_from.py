8fa2dee88a5ea4720d65699a0e5124d3f5348cca
RickMelendez <rickmelendez001@gmail.com>
2024-05-22 22:23:46 -0400

    changed the issubclass for isinstance

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -9,7 +9,7 @@ def inherits_from(obj, a_class):
     Returns True if obj is an instance of a subclass of a_class; else False.
         """
 
-    if type(obj) is a_class or not issubclass(obj, a_class):
+    if type(obj) is a_class or not isinstance(obj, a_class):
         return True
     else:
         return False

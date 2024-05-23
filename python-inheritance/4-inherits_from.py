c747d1ea775db72271ce9924e6baab5f828434bc
RickMelendez <rickmelendez001@gmail.com>
2024-05-22 22:28:35 -0400

    switch the true and false

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -10,6 +10,6 @@ def inherits_from(obj, a_class):
         """
 
     if type(obj) is a_class or not isinstance(obj, a_class):
-        return True
-    else:
         return False
+    else:
+        return True

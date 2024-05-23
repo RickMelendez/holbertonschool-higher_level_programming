6573197fa4dab7bfa4d1d638e43d18e2960057fe
RickMelendez <rickmelendez001@gmail.com>
2024-05-22 23:53:39 -0400

    initial commit

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -12,13 +12,14 @@ class BaseGeometry:
         """
         Function not implemented yet
             """
-        raise Exception(area() is not implemented)
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """
         Function that validates `value`
             """
         if type(value) != int:
-            raise TypeError(name + "must be an integer")
+            raise TypeError(name + " must be an integer")
+
         if value <= 0:
-            raise ValueError(name + "must be greater than 0")
+            raise ValueError(name + " must be greater than 0")

e59779737d22ba852fb498629c3ccd4810f87947
RickMelendez <rickmelendez001@gmail.com>
2024-05-29 00:25:54 -0400

    added comments

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+"""number_of_lines
+"""
+
+
 def number_of_lines(filename=""):
-    with open(filename, encoding='utf-8') as f:
-        return len(f.readlines())
+    """Takes in str filename to read the number of lines
+    """
+
+    with open(filename, encoding="utf-8") as readFile:
+        lines = 0
+        while True:
+            line = readFile.readline()
+            if not line:
+                break
+            lines += 1
+        return lines

44e5a61cee708ebbfa1b16dacd210ad8f8dd68a5
RickMelendez <rickmelendez001@gmail.com>
2024-05-29 00:10:23 -0400

    hot fix

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python3
+"""pascal_triangle
+"""
+
+
 def pascal_triangle(n):
+    """ends of each list in the matrix are summed to display the next list
+    until the middle of the list. The number '1' is always at both ends.
+    """
     if n <= 0:
         return []
 
-    limit = n - 1
-    triangle = [[1]]
-
-    for i in range(limit):
+    res = []
+    for elem in range(n):
+        if elem == 0:
+            res.append([1])
+            continue
+        if elem == 1:
+            res.append([1, 1])
+            continue
         row = []
-        row.append(1)
-
-        if len(triangle[i]) > 1:
-            prev_row_len = len(triangle[i]) - 1
-            nxt = 1
-
-            for j in range(prev_row_len):
-                suma = triangle[i][j] + triangle[i][nxt]
-                row.append(suma)
-                nxt += 1
-
-        row.append(1)
-        triangle.append(row)
-
-    return triangle
+        # init row
+        for item in range(elem + 1):
+            row.append(item)
+        for item in range(1, elem):
+            row[0] = 1
+            row[elem] = 1
+            row[item] = res[elem - 1][item] + res[elem - 1][item - 1]
+        res.append(row)
+            
+    return res

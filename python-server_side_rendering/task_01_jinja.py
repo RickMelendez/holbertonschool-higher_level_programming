f75ec6df394211169217ee22d33cf1502cbd03f0
RickMelendez <rickmelendez001@gmail.com>
2024-07-16 11:02:17 -0400

    update files

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import json
+import os
 
 app = Flask(__name__)
 
@@ -13,6 +15,15 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/items')
+def items():
+    # Load items from JSON file
+    with open('items.json', 'r') as f:
+        data = json.load(f)
+    items_list = data.get("items", [])
+    
+    return render_template('items.html', items=items_list)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
